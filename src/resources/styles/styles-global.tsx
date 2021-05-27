be279196e126d77d7d3d0bcde5d0bb7d04c317ed
akolodko <temakolodko@gmail.com>
2021-05-27 10:37:14 +0300

    Update styles, reduce images and font sizes

--- src/resources/styles/styles-global.tsx
+++ src/resources/styles/styles-global.tsx
@@ -13,7 +13,7 @@ const StyleGlobal = createGlobalStyle`
     color: #0a0606;
     font-family: Cairo, Helvetica, Arial, sans-serif;
     font-variant: normal;
-    background: #76cdd7;
+    background: #3277aa;
   }
   
   //html, body {
