dc1a6d9ef648f4b02ab0d29a3c5b34f32911405c
akolodko <temakolodko@gmail.com>
2021-07-06 20:43:03 +0300

    Add test video

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import BgWater from '../resources/images/bg-water.mp4'
+import BgWater from '../resources/images/bg-water-test.mp4'
 import React, { useRef } from 'react'
 import { useEffect } from 'react'
 
