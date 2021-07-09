28534a8082e0f21356ef52f48f31b7fc1553d4ff
akolodko <temakolodko@gmail.com>
2021-07-09 09:23:56 +0300

    CSS update

--- src/modules/account/Account.tsx
+++ src/modules/account/Account.tsx
@@ -30,7 +30,7 @@ const CardsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: calc(45% - 260px);
+  margin-top: 150px;
   cursor: pointer;
 `
 
