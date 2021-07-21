9863abf84aa936e2917cf7d3820361c88b61817e
akolodko <temakolodko@gmail.com>
2021-07-21 08:19:17 +0300

    WE-7032 Improve landscape styles

--- src/modules/account/Account.tsx
+++ src/modules/account/Account.tsx
@@ -35,10 +35,13 @@ const CardsContainer = styled.div`
   cursor: pointer;
   
   @media only screen 
-  and (min-device-width: 375px) 
-  and (max-device-width: 812px) {
-    transform: scale(0.8);
+  {
+    transform: scale(0.9);
     -webkit-tap-highlight-color: transparent;
+  }
+  
+  @media (orientation: landscape) {
+    transform: scale(0.8);
   }
 `
 
