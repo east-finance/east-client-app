5534d3209ffc8de3aa34296c523d31c93de6c914
akolodko <temakolodko@gmail.com>
2021-09-06 15:51:28 +0300

    WE-7400 Fix total westAmount in Mint form

--- src/modules/account/modals/buy-east/FillForm.tsx
+++ src/modules/account/modals/buy-east/FillForm.tsx
@@ -135,7 +135,7 @@ export const FillForm = observer((props: IProps) => {
   const westAvailable = roundNumber(+dataStore.westBalance - +totalFee, 8)
   const buyOptions = [{text: '25%', value: '0.25' }, { text: '50%', value: '0.5' }, { text: '75%', value: '0.75' }, { text: '100%', value: '1' }]
   const onSelectOption = (tag: ITag) => {
-    const amount = roundNumber(+tag.value * +dataStore.westBalance, 8).toString()
+    const amount = roundNumber(+tag.value * +westAvailable, 8).toString()
     setWestAmount(amount)
     setEastByWestAmount(amount)
   }
